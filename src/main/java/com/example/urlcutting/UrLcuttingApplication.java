1ce0f60716e4ad641a74ea712d58f8effa368914
DaniilDzeviatsen <deviat.daan@gmail.com>
2023-06-22 18:35:39 +0300

    Initial commit

---
+++ src/main/java/com/example/urlcutting/UrLcuttingApplication.java
@@ -0,0 +1,13 @@
+package com.example.urlcutting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrLcuttingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrLcuttingApplication.class, args);
+	}
+
+}
