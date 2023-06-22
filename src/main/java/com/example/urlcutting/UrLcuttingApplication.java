4345ac54362b4c93af3f31bba6df56ab43125fba
DaniilDzeviatsen <deviat.daan@gmail.com>
2023-06-22 18:51:55 +0300

    with models and database

--- src/main/java/com/example/urlcutting/UrLcuttingApplication.java
+++ src/main/java/com/example/urlcutting/UrLcuttingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UrLcuttingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UrLcuttingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UrLcuttingApplication.class, args);
+    }
 
 }
