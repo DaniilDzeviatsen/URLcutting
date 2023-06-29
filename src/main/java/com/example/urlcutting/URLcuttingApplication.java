a965bec90a50a6a11b065adac31ce582968c830a
DaniilDzeviatsen <deviat.daan@gmail.com>
2023-06-29 13:41:42 +0300

    with flyway

--- src/main/java/com/example/urlcutting/URLcuttingApplication.java
+++ src/main/java/com/example/urlcutting/URLcuttingApplication.java
@@ -1,5 +1,6 @@
 package com.example.urlcutting;
 
+import com.example.urlcutting.repository.UrlJdbcRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -11,7 +12,8 @@ public class URLcuttingApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(URLcuttingApplication.class, args);
-
+UrlJdbcRepository urlJdbcRepository=context.getBean(UrlJdbcRepository.class);
+urlJdbcRepository.addNewLink("mpdewd");
 
     }
 }
